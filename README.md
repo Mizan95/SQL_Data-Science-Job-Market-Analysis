80fca5c0ca21032f2823e174417ea148486f70bf
Mizan Rahman <113473772+Mizan95@users.noreply.github.com>
2025-11-25 16:03:16 +0000

    completed analysis of query 5 and included result set limited to 20

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ top skills classified by salary (AVG salary year)
 5. What are the most optimal skills to learn?
 highest job counts and highest paid
     * Optimal: High Demand and High Paying
+
+    Python is the single most valuable skill, leading the market with a total combined salary of approximately $595.3 million across high-paying jobs.
+The foundational trio of Python, SQL ($436.2 million), and R ($336.0 million) collectively account for the vast majority of the data science market's aggregate financial value.
+The high Total Combined Salary for these top skills is driven by a massive volume of job postings, rather than extremely high individual salaries.
+All top skills—Python, SQL, R, Tableau, and SAS—are associated with a consistently high average salary range (mostly between $122,000 and $138,500).
+This ranking highlights the essential skills required for the broadest and most lucrative segment of the data science job market in 2023.
+
--- project_sql/5. top 20 optimal skills for data scientist.sql
+++ project_sql/5. top 20 optimal skills for data scientist.sql
@@ -44,3 +44,117 @@ GROUP BY
 ORDER BY number_of_jobs DESC
 LIMIT
     20
+
+/*
+Analysis
+- Foundational Skills Dominate Market Value: Python, SQL, and R are the core requirements, accounting for the vast majority of jobs and the largest aggregate financial value (Python's total salary is approx $595.3M).
+
+- Specialisation Commands Premium Pay: Skills like Go ($147,466 average) and PyTorch ($145,989 average) offer the highest average salaries, suggesting a premium for specialized knowledge in niche or highly technical areas (e.g., performance, deep learning).
+
+- Low Demand, High Reward: The top-paying skills appear in significantly fewer jobs (e.g., Go with 316 jobs) compared to the essential skills (e.g., Python with 4,312 jobs).
+
+- Cloud/ML Frameworks are Mid-Tier: Cloud and machine learning tools like Spark ($144,399 average) and TensorFlow ($143,440 average) bridge the gap, offering high average salaries while still being required in a substantial number of jobs.
+
+[
+  {
+    "total_salary": "595267751.56640625",
+    "number_of_jobs": "4312",
+    "skill_name": "python"
+  },
+  {
+    "total_salary": "436194498.89453125",
+    "number_of_jobs": "3151",
+    "skill_name": "sql"
+  },
+  {
+    "total_salary": "336021244.51171875",
+    "number_of_jobs": "2486",
+    "skill_name": "r"
+  },
+  {
+    "total_salary": "168230566.0703125",
+    "number_of_jobs": "1278",
+    "skill_name": "tableau"
+  },
+  {
+    "total_salary": "151178759.0781250",
+    "number_of_jobs": "1230",
+    "skill_name": "sas"
+  },
+  {
+    "total_salary": "141083006.3281250",
+    "number_of_jobs": "1016",
+    "skill_name": "aws"
+  },
+  {
+    "total_salary": "136601278.2421875",
+    "number_of_jobs": "946",
+    "skill_name": "spark"
+  },
+  {
+    "total_salary": "91944925.3515625",
+    "number_of_jobs": "641",
+    "skill_name": "tensorflow"
+  },
+  {
+    "total_salary": "82794848.5000000",
+    "number_of_jobs": "623",
+    "skill_name": "azure"
+  },
+  {
+    "total_salary": "76873608.7890625",
+    "number_of_jobs": "617",
+    "skill_name": "excel"
+  },
+  {
+    "total_salary": "82130354.4921875",
+    "number_of_jobs": "602",
+    "skill_name": "hadoop"
+  },
+  {
+    "total_salary": "82337830.5078125",
+    "number_of_jobs": "564",
+    "skill_name": "pytorch"
+  },
+  {
+    "total_salary": "72800375.4218750",
+    "number_of_jobs": "557",
+    "skill_name": "java"
+  },
+  {
+    "total_salary": "57996954.6718750",
+    "number_of_jobs": "489",
+    "skill_name": "power bi"
+  },
+  {
+    "total_salary": "66507256.781250",
+    "number_of_jobs": "481",
+    "skill_name": "pandas"
+  },
+  {
+    "total_salary": "55576609.140625",
+    "number_of_jobs": "392",
+    "skill_name": "scikit-learn"
+  },
+  {
+    "total_salary": "55266509.968750",
+    "number_of_jobs": "381",
+    "skill_name": "scala"
+  },
+  {
+    "total_salary": "46352107.5625000",
+    "number_of_jobs": "376",
+    "skill_name": "git"
+  },
+  {
+    "total_salary": "46348237.21875",
+    "number_of_jobs": "339",
+    "skill_name": "numpy"
+  },
+  {
+    "total_salary": "46599158.578125",
+    "number_of_jobs": "316",
+    "skill_name": "go"
+  }
+]
+*/
