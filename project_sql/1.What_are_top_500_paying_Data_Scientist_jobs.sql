92087f5162c96d19a9ea1b8c8c38b7395b96d88d
Mizan Rahman <113473772+Mizan95@users.noreply.github.com>
2025-11-25 12:55:51 +0000

    changed query 1 to global location. added analysis and result set in JSON format at end of query 2

--- project_sql/1.What_are_top_500_paying_Data_Scientist_jobs.sql
+++ project_sql/1.What_are_top_500_paying_Data_Scientist_jobs.sql
@@ -1,60 +1,7 @@
 /* ❓What are the 500 top paying Data Scientist jobs in the UK and which companies are hiring for these roles?
-I will return the above analysis with two approaches in our SQL queries. After both approaches is my conclusion describing which
-approach is best practice.
-The first approach is by using a CTE.
-And the second is without.
 Comments are visible to explain the logic.
 */
 
-
-/*
-*-*-*-*- Below is the first approach using a CTE *-*-*-*-
-*/
-
-/*
-This query is split up into two blocks. The first block is the CTE. Whilst the second block is the main query.
-*/
-
--- CTE returning company name and ID from company Dimension table (company_dim)
-WITH company_list AS (
-    SELECT
-    company_id,
-    name
-    FROM
-    company_dim
-)
-
-/* Main query which: 
-1. Merges the Fact table (job_postings_fact) with CTE (company_list) using inner join. The primary key of the CTE is company_id. This matches with the foreign key of
-job_postings_fact which is also company_id.
-2. Filters out NULL values and selecting only Data Scientist jobs
-3. Returns top 500 Data Scientist jobs along with company names and yearly salaries
-*/
-
-SELECT
-    job_title_short AS job_title,
-    company_list.name AS company_name,
-    job_country AS country,
-    salary_year_avg AS salary
-FROM
-    job_postings_fact AS job_postings
--- Inner join with CTE to retrieve company name
-INNER JOIN company_list ON job_postings.company_id = company_list.company_id
--- Filter out NULL value in salary and only choose Data Scientist jobs
-WHERE
-    salary_year_avg IS NOT NULL AND
-    job_title_short = 'Data Scientist' AND
-    job_location = 'United Kingdom'
--- Show results in descending order based on salaries
-ORDER BY
-    salary DESC
--- Limit result set to 500
-LIMIT
-    500;
-
-/*
-*-*-*-*- Below is the second approach without a CTE *-*-*-*-
-*/
 
 /*
 1. This query consists of a single block and merges the Fact table (job_postings_fact) with the company Dimension table (company_dim) directly
@@ -64,6 +11,7 @@ job_postings_fact which is also company_id.
 3. The query then selects the appropriate columns from both tables whilst filtering out NULL values selecting only Data Scientist jobs
 4. This returns the top 500 Data Scientist jobs along with company names and yearly salaries
 */
+
 SELECT 
     job_title_short AS job_title,
     company_dim.name AS company_name,
@@ -76,8 +24,7 @@ INNER JOIN company_dim ON job_postings.company_id = company_dim.company_id
 WHERE 
 -- Filter out NULL value in salary and only choose Data Scientist jobs
     job_postings.salary_year_avg IS NOT NULL AND
-    job_postings.job_title_short = 'Data Scientist' AND
-    job_location = 'United Kingdom'
+    job_postings.job_title_short = 'Data Scientist'
 -- Show results in descending order based on salaries
 ORDER BY
     salary DESC
--- project_sql/2.What_skills_required_top_jobs.sql
+++ project_sql/2.What_skills_required_top_jobs.sql
@@ -19,10 +19,10 @@ WITH skills_job_table AS (
 -- CTE 2: Selects the top 500 job IDs for 'Data Scientist' roles based  on the highest yearly salary.
 top_data_scientist_jobs AS (
     SELECT
-    job_id,
-    job_title_short AS job_title,
-    salary_year_avg AS salary,
-    job_country
+        job_id,
+        job_title_short AS job_title,
+        salary_year_avg AS salary,
+        job_country
     FROM job_postings_fact
     WHERE
         job_title_short = 'Data Scientist' AND
@@ -42,7 +42,7 @@ SELECT
     COUNT(top_data_scientist_jobs.job_id) AS number_of_jobs
 FROM
     top_data_scientist_jobs
-LEFT OUTER JOIN skills_job_table ON top_data_scientist_jobs.job_id = skills_job_table.job_id
+INNER JOIN skills_job_table ON top_data_scientist_jobs.job_id = skills_job_table.job_id
 WHERE
     skill_name IS NOT NULL
 GROUP BY
@@ -51,3 +51,115 @@ ORDER BY
     number_of_jobs DESC
 LIMIT
     20;
+
+/*
+Analysis
+Python is the most demanded skill, appearing in 358 of the job postings.
+SQL is the second most critical skill, mentioned in 279 job listings.
+R rounds out the top three languages, required by 189 jobs.
+Spark leads the specialized tools, demanded by 101 job postings for Big Data processing.
+Tableau and AWS are close, with 84 and 77 mentions, highlighting visualization and cloud infrastructure needs.
+
+[
+  {
+    "ranking": "1",
+    "skill_name": "python",
+    "number_of_jobs": "358"
+  },
+  {
+    "ranking": "2",
+    "skill_name": "sql",
+    "number_of_jobs": "279"
+  },
+  {
+    "ranking": "3",
+    "skill_name": "r",
+    "number_of_jobs": "189"
+  },
+  {
+    "ranking": "4",
+    "skill_name": "spark",
+    "number_of_jobs": "101"
+  },
+  {
+    "ranking": "5",
+    "skill_name": "tableau",
+    "number_of_jobs": "84"
+  },
+  {
+    "ranking": "6",
+    "skill_name": "aws",
+    "number_of_jobs": "77"
+  },
+  {
+    "ranking": "7",
+    "skill_name": "tensorflow",
+    "number_of_jobs": "64"
+  },
+  {
+    "ranking": "8",
+    "skill_name": "pytorch",
+    "number_of_jobs": "57"
+  },
+  {
+    "ranking": "9",
+    "skill_name": "scala",
+    "number_of_jobs": "49"
+  },
+  {
+    "ranking": "10",
+    "skill_name": "hadoop",
+    "number_of_jobs": "41"
+  },
+  {
+    "ranking": "11",
+    "skill_name": "scikit-learn",
+    "number_of_jobs": "40"
+  },
+  {
+    "ranking": "12",
+    "skill_name": "sas",
+    "number_of_jobs": "38"
+  },
+  {
+    "ranking": "13",
+    "skill_name": "pandas",
+    "number_of_jobs": "35"
+  },
+  {
+    "ranking": "14",
+    "skill_name": "snowflake",
+    "number_of_jobs": "34"
+  },
+  {
+    "ranking": "15",
+    "skill_name": "go",
+    "number_of_jobs": "34"
+  },
+  {
+    "ranking": "16",
+    "skill_name": "excel",
+    "number_of_jobs": "33"
+  },
+  {
+    "ranking": "17",
+    "skill_name": "azure",
+    "number_of_jobs": "33"
+  },
+  {
+    "ranking": "18",
+    "skill_name": "c",
+    "number_of_jobs": "27"
+  },
+  {
+    "ranking": "19",
+    "skill_name": "numpy",
+    "number_of_jobs": "27"
+  },
+  {
+    "ranking": "20",
+    "skill_name": "java",
+    "number_of_jobs": "25"
+  }
+]
+*/
